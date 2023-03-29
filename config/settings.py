dbe138e00c7958b3649c3c960091c1b7c08fec5c
Yoiner Duran <duranyoiner@gmail.com>
2023-03-28 22:27:39 -0500

    url login

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 STATIC_URL = 'static/'
+LOGIN_URL = 'iniciosesion'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
